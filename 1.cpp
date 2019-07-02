e53bfb5aa243264bbf5364a69e046ca0f8e9eb0f
bafear <52194492+bafear@users.noreply.github.com>
2019-07-02 15:58:54 +0700

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float C;
+	cout<< "Enter Cels temperature: ";
+	cin	>> C;
+	cout<<"Fahrenheit = " << ((C*9)/5)+32<< endl;
+	cout<<"Now weather in Thailand is " << ((((C*9)/5)+32<70) == 0 ? "HOT" : "COOL") << endl;
+	return 0;
+}
