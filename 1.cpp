913a4cc8f3a6bb031d03a1aaa562ed7e0276f75a
bafear <52194492+bafear@users.noreply.github.com>
2019-07-09 13:41:04 +0700

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -6,6 +6,6 @@ int main()
 	cout<< "Enter Cels temperature: ";
 	cin	>> C;
 	cout<<"Fahrenheit = " << ((C*9)/5)+32<< endl;
-	cout<<"Now weather in Thailand is " << ((((C*9)/5)+32<70) == 0 ? "HOT" : "COOL") << endl;
+	cout<<"Now weather in Thailand is " << ((((C*9)/5)+32<70) ? "HOT" : "COOL") << endl;
 	return 0;
 }
